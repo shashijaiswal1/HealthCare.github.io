79125bc82fd1565e67d64777afa296b5ad525f3c
Shashi Jaiswal <shashi.jaiswal.che@gmail.com>
2021-06-27 20:50:38 +0530

    Initial commit

---
+++ HealthCare.js
@@ -0,0 +1,29 @@
+
+    // let para = document.getElementById('para');
+    // para.addEventListener('mouseover', function run() {
+    //     console.log('Mouse Inside');
+    //     alert("in");
+    // });
+
+    // para.addEventListener('mouseout', function run() {
+    //     console.log('Mouse outside');
+    // });
+
+    // function hideshow() {
+    //     let para = document.getElementById('para');
+    //     if (para.style.display != 'none') {
+    //         para.style.display = 'none';
+    //     }
+    //     else {
+    //         para.style.display = 'block';
+
+    //     }
+    // }
+
+<script>
+$(document).ready ( function () {
+    $("#ImageHosting").click(function () {
+       alert("test");
+    });
+});
+    </script>
